7db29cc1a0315bbbfb9c3ab1a34f90c23d3359ce
trulsrane <truls.rane@gmail.com>
2026-09-17 16:45:06 +0200

    Tog bort tab komponenterna och flyttade till /app istället
    Blev konstigt att hantera tabs utanför /app så tog bort komponenterna och la tillbaka tabs strukturen under /app istället, vill vi ha webb senare så kan vi lägga om det. Skillnaden va att förut använde den fallback om appen öppnades på en webb, nu finns inte den men vi planerar ändå inte att utveckla den för webb inom ramen för kursen. Enkelt att byta sen ändå om det blir relevant

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,5 +1,16 @@
-import { AppTabs } from '../../components/app-tabs';
+import { NativeTabs } from 'expo-router/unstable-native-tabs';
 
 export default function TabLayout() {
-  return <AppTabs />;
+  return (
+    <NativeTabs minimizeBehavior="onScrollDown">
+      <NativeTabs.Trigger name="index">
+        <NativeTabs.Trigger.Icon sf={{ default: 'house', selected: 'house.fill' }} />
+        <NativeTabs.Trigger.Label>Tab One</NativeTabs.Trigger.Label>
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="two">
+        <NativeTabs.Trigger.Icon sf={{ default: 'map', selected: 'map.fill' }} />
+        <NativeTabs.Trigger.Label>Tab Two</NativeTabs.Trigger.Label>
+      </NativeTabs.Trigger>
+    </NativeTabs>
+  );
 }
