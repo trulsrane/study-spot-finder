152f297e81252e048628f3769a83668ac842fce9
trulsrane <truls.rane@gmail.com>
2026-09-17 08:48:42 +0200

    Initial commit
    Generated by create-expo-stack 2.23.3

---
+++ components/Container.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export const Container = ({ children }: { children: React.ReactNode }) => {
+  return <SafeAreaView style={styles.container}>{children}</SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: 'white',
+  },
+});
